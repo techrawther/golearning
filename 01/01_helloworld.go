720e55e3abd0f152cc4b000c968ea62929b12e9f
Shah Rawther <techrawther@gmail.com>
2017-12-06 16:35:18 +0000

    First commit

---
+++ 01/01_helloworld.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 200; i++ {
+		fmt.Printf("%d - %b %#x %#X \n", i, i, i, i)
+	}
+
+}
+
+func testmain() {
+
+	fmt.Printf("This is Samih")
+}
+
